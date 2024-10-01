eab04ea12a8b169fc535df6e609371c279da1a49
Mayank <mayankpatil2605@gmail.com>
2024-10-01 00:17:50 -0700

    Added languageSelect and Inputtext to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextInput from './TextInput';
+import LanguageSelect from './LanguageSelect';
 
 const App: React.FC = () => {
-    const [text, setText]
+    const [text, setText] = useState<string>(''); // user input text
+    const [sourceLang, setSourcelang] = useState<string>('en'); // set Source language to english
+    const [targetLang, setTargetLang] = useState<string>('es'); // set Target language to spanish
+
   return (
-    
+    <div className="App">
+        <h1> Langauge Translator </h1>
+        
+        {/* Text Input */}
+        <TextInput text={text} onTextChange={ (e) =>
+            setText(e.target.value)}/>
+
+        {/* Source Language Selection*/}
+        <LanguageSelect
+        label="Translate from:"
+        selectedLanguage={sourceLang}
+        onLanguageChange={(e) => setSourcelang(e.target.value)}
+        />
+
+        {/* Target language Selection */}
+        <LanguageSelect
+        label="Translate to:"
+        selectedLanguage={targetLang}
+        onLanguageChange={(e) => setTargetLang(e.target.value)}
+        />
+    </div>
   )
 }
 
