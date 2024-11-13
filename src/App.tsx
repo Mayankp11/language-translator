c3daa140d08857d25e23909e77082e568ccee16e
Mayank <mayankpatil2605@gmail.com>
2024-11-13 11:43:41 -0800

    testing vercel 2

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ const App: React.FC = () => {
               bg: "green.100",
             }}
           >
-            TranslateMayank
+            TranslateMate
           </Highlight>
         </Heading>
       </Box>
