8f794b0291d477048bbec9db933952b06892aa85
Mayank <mayankpatil2605@gmail.com>
2024-11-13 08:24:11 -0800

    Added a Grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import TextInput from "./components/TextInput";
 import LanguageSelect from "./components/LanguageSelect";
 import TranslationLogic from "./services/TranslationLogic";
-import { Box, Flex, Heading, Highlight, Text } from "@chakra-ui/react";
+import { Flex, Highlight, Text } from "@chakra-ui/react";
 import TranslatedText from "./components/TranslatedText";
 import RefreshButton from "./components/RefreshButton";
 import AppBackground from "./components/AppBackground";
+import { Box, Container, Grid, GridItem, Heading } from "@chakra-ui/react";
 
 const App: React.FC = () => {
   const [text, setText] = useState<string>("");
@@ -33,105 +34,149 @@ const App: React.FC = () => {
     }, 500);
   };
 
+  //   return (
+  //     <>
+  //       <AppBackground />
+  //       <Heading as="h1" size="4xl" textAlign="center" mb={5} mt={7}>
+  //         <Highlight
+  //           query="Mate"
+  //           styles={{
+  //             px: "2",
+  //             py: "1",
+  //             rounded: "full",
+  //             bg: "green.100",
+  //           }}
+  //         >
+  //           TranslateMate
+  //         </Highlight>
+  //       </Heading>
+
+  //       <Box
+  //         mt="40px"
+  //         p={{ base: 2, md: 4 }}
+  //         bg="#f0f8ff"
+  //         borderRadius="md"
+  //         borderWidth={1}
+  //         borderColor="black"
+  //         boxShadow="md"
+  //         width="100%"
+  //         maxWidth={{ base: "100%", md: "600px" }}
+  //         minHeight={{ base: "200px", md: "300px" }}
+  //         margin="0 auto"
+  //         color="black"
+  //       >
+  //         {error && (
+  //           <Box bg="red.200" p={2} borderRadius="md" mb={4}>
+  //             <Text color="red.800">{error}</Text>
+  //           </Box>
+  //         )}
+
+  //         <Flex
+  //           direction={{ base: "column", md: "row" }}
+  //           alignItems="stretch"
+  //           justify="space-between"
+  //           mt={5}
+  //           mb={{ base: 2, md: 0 }}
+  //         >
+  //           <Flex direction="column" flex={1} mr={{ base: 0, md: 4 }} mb={{ base: 2, md: 0 }}>
+  //             <Box mb={1} flex="1" width={{ base: "100%", md: "150px" }}>
+  //               <LanguageSelect
+  //                 label="Translate from:"
+  //                 selectedLanguage={sourceLang}
+  //                 onLanguageChange={(e) => setSourceLang(e.target.value)}
+  //               />
+  //             </Box>
+
+  //             <Box
+  //               mb={1}
+  //               flex="2"
+  //               width={{ base: "100%", md: "250px" }}
+  //               minHeight={{ base: "200px", md: "300px" }}
+  //               boxSizing="border-box"
+  //             >
+  //               <TextInput text={text} onTextChange={(e) => setText(e.target.value)} />
+  //             </Box>
+  //           </Flex>
+
+  //           <Box mx={{ base: 0, md: 4 }} />
+
+  //           <Flex direction="column" flex={1} mb={{ base: 2, md: 0 }}>
+  //             <Box mb={1} flex="1" width={{ base: "100%", md: "150px" }}>
+  //               <LanguageSelect
+  //                 label="Translate to:"
+  //                 selectedLanguage={targetLang}
+  //                 onLanguageChange={(e) => setTargetLang(e.target.value)}
+  //               />
+  //             </Box>
+
+  //             <Box
+  //               flex="2"
+  //               width={{ base: "100%", md: "250px" }}
+  //               minHeight={{ base: "200px", md: "300px" }}
+  //               boxSizing="border-box"
+  //             >
+  //               <TranslatedText translatedText={translatedText} />
+  //             </Box>
+  //           </Flex>
+  //         </Flex>
+
+  //         {/* Translation Logic */}
+  //         <TranslationLogic
+  //           text={text}
+  //           sourceLang={sourceLang}
+  //           targetLang={targetLang}
+  //           onTranslated={handleTraslationResult}
+  //           onError={(errorMsg) => setError(errorMsg)} // Set error if translation fails
+  //         />
+
+  //         <RefreshButton onRefresh={handleRefresh} isLoading={isLoading} />
+  //       </Box>
+  //     </>
+  //   );
+  // };
+
+  // export default App;
+
+  // function App() {
   return (
-    <>
-      <AppBackground />
-      <Heading as="h1" size="4xl" textAlign="center" mb={5} mt={7}>
-        <Highlight
-          query="Mate"
-          styles={{
-            px: "2",
-            py: "1",
-            rounded: "full",
-            bg: "green.100",
-          }}
-        >
-          TranslateMate
-        </Highlight>
-      </Heading>
-
-      <Box
-        mt="40px"
-        p={{ base: 2, md: 4 }}
-        bg="#f0f8ff"
-        borderRadius="md"
-        borderWidth={1}
-        borderColor="black"
-        boxShadow="md"
-        width="100%"
-        maxWidth={{ base: "100%", md: "600px" }}
-        minHeight={{ base: "200px", md: "300px" }}
-        margin="0 auto"
-        color="black"
-      >
-        {error && (
-          <Box bg="red.200" p={2} borderRadius="md" mb={4}>
-            <Text color="red.800">{error}</Text>
-          </Box>
-        )}
-
-        <Flex
-          direction={{ base: "column", md: "row" }}
-          alignItems="stretch"
-          justify="space-between"
-          mt={5}
-          mb={{ base: 2, md: 0 }}
-        >
-          <Flex direction="column" flex={1} mr={{ base: 0, md: 4 }} mb={{ base: 2, md: 0 }}>
-            <Box mb={1} flex="1" width={{ base: "100%", md: "150px" }}>
-              <LanguageSelect
-                label="Translate from:"
-                selectedLanguage={sourceLang}
-                onLanguageChange={(e) => setSourceLang(e.target.value)}
-              />
-            </Box>
-
-            <Box
-              mb={1}
-              flex="2"
-              width={{ base: "100%", md: "250px" }}
-              minHeight={{ base: "200px", md: "300px" }}
-              boxSizing="border-box"
-            >
-              <TextInput text={text} onTextChange={(e) => setText(e.target.value)} />
-            </Box>
-          </Flex>
-
-          <Box mx={{ base: 0, md: 4 }} />
-
-          <Flex direction="column" flex={1} mb={{ base: 2, md: 0 }}>
-            <Box mb={1} flex="1" width={{ base: "100%", md: "150px" }}>
-              <LanguageSelect
-                label="Translate to:"
-                selectedLanguage={targetLang}
-                onLanguageChange={(e) => setTargetLang(e.target.value)}
-              />
-            </Box>
-
-            <Box
-              flex="2"
-              width={{ base: "100%", md: "250px" }}
-              minHeight={{ base: "200px", md: "300px" }}
-              boxSizing="border-box"
-            >
-              <TranslatedText translatedText={translatedText} />
-            </Box>
-          </Flex>
-        </Flex>
-
-
-        {/* Translation Logic */}
-        <TranslationLogic
-          text={text}
-          sourceLang={sourceLang}
-          targetLang={targetLang}
-          onTranslated={handleTraslationResult}
-          onError={(errorMsg) => setError(errorMsg)} // Set error if translation fails
-        />
-
-        <RefreshButton onRefresh={handleRefresh} isLoading={isLoading} />
+    <Container maxW="container.lg" p={4}>
+      {/* Nav */}
+      <Box as="header" bg="teal.500" p={4} color="white" textAlign="center">
+        <AppBackground />
+        <Heading as="h1" size="4xl" textAlign="center" mb={5} mt={7}>
+          <Highlight
+            query="Mate"
+            styles={{
+              px: "2",
+              py: "1",
+              rounded: "full",
+              bg: "green.100",
+            }}
+          >
+            TranslateMate
+          </Highlight>
+        </Heading>
       </Box>
-    </>
+
+      {/* Main Layout */}
+      <Grid templateRows="repeat(2, 1fr)" gap={4} mt={4}>
+        {/* Main */}
+        <GridItem>
+          <Box bg="blue.100" p={6} borderRadius="md">
+            <Heading size="md">Main Content</Heading>
+            <p>Content goes here</p>
+          </Box>
+        </GridItem>
+
+        {/* Main2 */}
+        <GridItem>
+          <Box bg="red.100" p={6} borderRadius="md">
+            <Heading size="md">Main Content 2</Heading>
+            <p>More content goes here</p>
+          </Box>
+        </GridItem>
+      </Grid>
+    </Container>
   );
 };
 
